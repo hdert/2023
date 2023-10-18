46662567c934d247880ba9197fe09b57642849ed
hdert <40216616+hdert@users.noreply.github.com>
2023-10-19 10:26:31 +1300

    Create Stack implementation

---
+++ C/Stack/src/Stack.h
@@ -0,0 +1,21 @@
+#ifndef STACK_H
+#define STACK_H
+#include "../../LinkedList/src/LinkedList.h"
+
+typedef struct Stack
+{
+    struct Node *ptr;
+    int length;
+} Stack;
+
+void Stack_print(Stack STACK);
+
+void Stack_add(Stack *STACK, int value);
+
+int Stack_pop(Stack *STACK);
+
+int Stack_peek(Stack STACK);
+
+void Stack_free(Stack *STACK);
+
+#endif // STACK_H

90d8c80e6c85ac2900b091b8fdafecd9d3c0e42f
hdert <40216616+hdert@users.noreply.github.com>
2023-12-06 19:47:32 +1300

    Compact zig InfiniteProductCalculator code

--- Zig/Calculators/InfiniteProductCalculator/src/main.zig
+++ Zig/Calculators/InfiniteProductCalculator/src/main.zig
@@ -5,17 +5,10 @@ pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     var buffer: [100]u8 = undefined;
     var total: f64 = 1;
-
     try stdout.print("Type any amount of numbers you want the product of, finishing with a blank line:\n", .{});
-
     while (true) {
         const user_input = try stdin.readUntilDelimiterOrEof(buffer[0..], '\n') orelse break;
-        if (std.fmt.parseFloat(f64, user_input)) |number| {
-            total *= number;
-        } else |_| {
-            break;
-        }
+        total *= std.fmt.parseFloat(f64, user_input) catch break;
     }
-
     try stdout.print("The product is {d}\n", .{total});
 }

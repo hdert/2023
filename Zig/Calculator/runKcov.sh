3f5d305f8d320f928e22321301b9147d3941a2e3
hdert <40216616+hdert@users.noreply.github.com>
2023-12-23 01:48:59 +1300

    zig calc: kcov update excluded paths

--- Zig/Calculator/runKcov.sh
+++ Zig/Calculator/runKcov.sh
@@ -3,5 +3,5 @@
 rm -r zig-cache
 rm -r kcov-output
 zig build test
-kcov --exclude-path=/usr/lib/zig/lib/,src/CalculatorLibTests.zig,../Stack/src/,src/Tokenizer.zig kcov-output zig-cache/o/*/test
+kcov --exclude-path=/usr/lib/zig/lib/,src/CalculatorLibTests.zig,src/Tokenizer.zig,src/CalculatorIo.zig,../Stack/src/ kcov-output zig-cache/o/*/test
 xdg-open kcov-output/index.html

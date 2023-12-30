978844519689512fcc9e035bb952f53055c440f3
hdert <40216616+hdert@users.noreply.github.com>
2023-12-30 16:40:20 +1300

    zig wasm website: switch to evaluate_quick and streaming for initiation

--- Zig/Calculator-Website/main.js
+++ Zig/Calculator-Website/main.js
@@ -2,39 +2,51 @@
 // and the freestanding target doesn't work, and we aren't passing the export
 // memory flag to the wasm-linker when building with zig.
 
+// import wasm from "./zig-out/Calculator/Calculator.wasm";
 
+const module = await WebAssembly.compileStreaming(
+  fetch("./zig-out/Calculator/Calculator.wasm")
+);
 
-request = new XMLHttpRequest();
-request.open('GET', 'zig-out/Calculator/Calculator.wasm');
-request.responseType = 'arraybuffer';
-request.send();
+// request = new XMLHttpRequest();
+// request.open("GET", "zig-out/Calculator/Calculator.wasm");
+// request.responseType = "arraybuffer";
+// request.send();
 
-request.onload = function() {
-    var bytes = request.response;
-    WebAssembly.instantiate(bytes, {
-        env: {
-            print: (result) => {console.log('${result}');},
-            // memory: new WebAssembly.Memory({initial: 16}),
-        },
-    }).then(result => {
-        const evaluate = result.instance.exports.evaluate_quick;
-        const allocateUint8 = result.instance.exports.allocateUint8;
-        const memory = result.instance.exports.memory;
-
-        const encodeString = (string) => {
-            const buffer = new TextEncoder().encode(string);
-            const pointer = allocateUint8(buffer.length + 1); // ask Zig to allocate memory
-            const slice = new Uint8Array(
-              memory.buffer, // memory exported from Zig
-              pointer,
-              buffer.length + 1
-            );
-            slice.set(buffer);
-            slice[buffer.length] = 0; // null byte to null-terminate the string
-            return pointer;
-          };
-        // console.log(result.instance.exports);
-
-        console.log(evaluate(encodeString("10+10"), 0));
-    });
+const encodeString = (string) => {
+  const buffer = new TextEncoder().encode(string);
+  const pointer = alloc(buffer.length + 1); // ask Zig to allocate memory
+  if (pointer == 0) {
+    throw allocationFailed;
+  }
+  const slice = new Uint8Array(
+    memory.buffer, // memory exported from Zig
+    pointer,
+    buffer.length + 1
+  );
+  slice.set(buffer);
+  slice[buffer.length] = 0; // null byte to null-terminate the string
+  return pointer;
 };
+
+const decodeString = (pointer, length) => {
+  const slice = new Uint8Array(
+    memory.buffer, //
+    pointer,
+    length
+  );
+  return new TextDecoder().decode(slice);
+};
+
+const {
+  exports: { memory, evaluate, alloc },
+} = await WebAssembly.instantiate(module, {
+  env: {
+    print: (pointer, length) => {
+      const string = decodeString(pointer, length);
+      console.log(`${string}`);
+    },
+  },
+});
+
+console.log(evaluate(encodeString("10+10")));
--- Zig/Calculator-Website/wasm.zig
+++ Zig/Calculator-Website/wasm.zig
@@ -1,27 +1,18 @@
 const std = @import("std");
 const Cal = @import("Calculator");
 const Addons = @import("Addons");
+const allocator = std.heap.wasm_allocator;
 
-pub const Result = extern struct {
-    result: Return,
-    tag: bool,
-};
+extern fn print([*:0]const u8, usize) void;
 
-pub const Return = extern union {
-    success: f64,
-    fail: u16,
-};
-
-extern fn print(u32) void;
-
-export fn allocateUint8(length: u32) [*]const u8 {
-    const slice = std.heap.wasm_allocator.alloc(u8, length) catch
-        @panic("failed to allocate memory");
-    return slice.ptr;
+export fn alloc(length: usize) ?[*]u8 {
+    return if (allocator.alloc(u8, length)) |slice|
+        slice.ptr
+    else |_|
+        null;
 }
 
-export fn evaluate_quick(input: [*:0]const u8, previousInput: f64) f64 {
-    const allocator = std.heap.wasm_allocator;
+export fn evaluate(input: [*:0]const u8, previousInput: f64) f64 {
     const slice = std.mem.span(input);
     defer allocator.free(slice);
 
@@ -39,49 +30,4 @@ export fn evaluate_quick(input: [*:0]const u8, previousInput: f64) f64 {
         null,
     ) catch return 0)
         .evaluate() catch return 0;
-}
-
-export fn evaluate(input: [*:0]const u8, previousInput: f64) Result {
-    const allocator = std.heap.wasm_allocator;
-    const slice = std.mem.span(input);
-    defer allocator.free(slice);
-    var equation = Cal.Equation.init(
-        allocator,
-        null,
-        null,
-    ) catch |err| {
-        return Result{
-            .result = .{ .fail = @intFromError(err) },
-            .tag = false,
-        };
-    };
-    defer equation.free();
-    Addons.registerKeywords(&equation) catch |err| {
-        return Result{
-            .result = .{ .fail = @intFromError(err) },
-            .tag = false,
-        };
-    };
-    equation.registerPreviousAnswer(previousInput) catch |err| {
-        return Result{
-            .result = .{ .fail = @intFromError(err) },
-            .tag = false,
-        };
-    };
-    const infixEquation = equation.newInfixEquation(slice, null) catch |err| {
-        return Result{
-            .result = .{ .fail = @intFromError(err) },
-            .tag = false,
-        };
-    };
-    const result = infixEquation.evaluate() catch |err| {
-        return Result{
-            .result = .{ .fail = @intFromError(err) },
-            .tag = false,
-        };
-    };
-    return Result{
-        .result = .{ .success = result },
-        .tag = true,
-    };
 }
